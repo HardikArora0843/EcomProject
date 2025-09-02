8a0a2045e433362ab2d3aba706b1f4cde2e706b2
Hardik Arora <118887883+HardikArora0843@users.noreply.github.com>
2025-09-03 03:49:24 +0530

    Update List.jsx

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
-import { Package, Search, Filter, Trash2, Edit, Eye, Crown, Star } from 'lucide-react'
+import { Package, Search, Filter, Trash2, Edit, Eye, Crown, Star,Plus } from 'lucide-react'
 
 const List = ({ token }) => {
   const [list, setList] = useState([])
