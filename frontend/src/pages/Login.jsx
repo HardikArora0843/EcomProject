fba313fb93d0a9e5e041d01cb0f5d61be829ab5e
Hardik Arora <118887883+HardikArora0843@users.noreply.github.com>
2025-09-03 04:02:15 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         if (response.data.success) {
           setToken(response.data.token)
           localStorage.setItem('token', response.data.token)
-          toast.success('Welcome to SanjayShop!')
+          toast.success('Welcome to EcomShop!')
         } else {
           toast.error(response.data.message)
         }
